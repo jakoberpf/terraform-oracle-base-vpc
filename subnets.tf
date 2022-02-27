e3676da360389d181cd88abb22f1368eb4fcf556
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-02-27 23:03:35 +0100

    shorten dnsLabel

--- subnets.tf
+++ subnets.tf
@@ -4,7 +4,7 @@ resource "oci_core_subnet" "private" {
   cidr_block                 = "192.168.7${index(var.availability_domains, each.value)}.0/24"
   display_name               = "${var.name}-private-subnet-${index(var.availability_domains, each.value)}-${random_string.deployment_id.result}"
   prohibit_public_ip_on_vnic = true
-  dns_label                  = "${var.name}Private${index(var.availability_domains, each.value)}"
+  dns_label                  = "${var.name}Pr${index(var.availability_domains, each.value)}"
   compartment_id             = var.compartment_id
   vcn_id                     = oci_core_vcn.this.id
   route_table_id             = oci_core_default_route_table.this.id
@@ -20,7 +20,7 @@ resource "oci_core_subnet" "public" {
   cidr_block                 = "192.168.8${index(var.availability_domains, each.value)}.0/24"
   display_name               = "${var.name}-public-subnet-${index(var.availability_domains, each.value)}-${random_string.deployment_id.result}"
   prohibit_public_ip_on_vnic = false
-  dns_label                  = "${var.name}Public${index(var.availability_domains, each.value)}"
+  dns_label                  = "${var.name}Pu${index(var.availability_domains, each.value)}"
   compartment_id             = var.compartment_id
   vcn_id                     = oci_core_vcn.this.id
   route_table_id             = oci_core_default_route_table.this.id
